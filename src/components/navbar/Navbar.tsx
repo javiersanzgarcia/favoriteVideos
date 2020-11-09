9709c015379efa286c6933fe5abc3c18068fd662
Javier Sanz <javi.sanz@keytree.es>
2020-11-09 20:08:54 +0100

    Deleting burguer menu in mobile view

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -8,29 +8,16 @@ const Navbar = () => {
                 <Link className="navbar-brand" to="/favoriteVideos/">
                     My Favorite Videos
                 </Link>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon" />
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <Link
-                                className="nav-link"
-                                to="/favoriteVideos/new-video"
-                            >
-                                Create New Video
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <Link
+                            className="nav-link"
+                            to="/favoriteVideos/new-video"
+                        >
+                            Create New Video
+                        </Link>
+                    </li>
+                </ul>
             </div>
         </nav>
     )
